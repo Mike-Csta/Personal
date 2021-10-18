bd87c999b28d06dfa7ffa3a14fc4c9120213dc23
Mike-Csta <92047998+Mike-Csta@users.noreply.github.com>
2021-10-18 20:48:23 +0200

    plus

--- src/About/About.js
+++ src/About/About.js
@@ -1,9 +1,22 @@
 import "./About.css";
+import karteczki from "../img/1.png";
 function About() {
   return (
-    <div className="container">
-      <div className="left"></div>
-      <div className="right"></div>
+    <div className="a-container">
+      <div className="a-left">
+        <div className="a-left-wrap">
+          <img src={karteczki} alt="photo" className="a-img" />
+        </div>
+      </div>
+      <div className="a-right">
+        <div className="a-right-wrap">
+          <h1 className="a-title">Prezydent</h1>
+          <div className="a-desc">
+            Polski prawnik i polityk, od 6 sierpnia 2015 Prezydent
+            Rzeczypospolitej Polskiej.
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -2,31 +2,31 @@ import "./Intro.css";
 import Me from "../img/andrzej.png";
 const Intro = () => {
   return (
-    <div className="container">
-      <div className="left">
-        <div className="left-wrapper">
-          <h2 className="intro">Helo, My name is</h2>
-          <h1 className="name">Andrzej Duda</h1>
-          <div className="title">
-            <div className="title-wrapper">
-              <div className="title-item">Web Developer</div>
-              <div className="title-item">UI/UX Designer</div>
-              <div className="title-item">Photographer</div>
-              <div className="title-item">Writer</div>
-              <div className="title-item">Contex-creator</div>
+    <div className="i-container">
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">Helo, My name is</h2>
+          <h1 className="i-name">Andrzej Duda</h1>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              <div className="i-title-item">Web Developer</div>
+              <div className="i-title-item">UI/UX Designer</div>
+              <div className="i-title-item">Photographer</div>
+              <div className="i-title-item">Writer</div>
+              <div className="i-title-item">Contex-creator</div>
               <div className="title-item">Web Developer</div>
             </div>
           </div>
-          <p className="desc">
+          <p className="i-desc">
             I design and develop services for customers of all sizes,
             specializing in creating stylish, modern websites, web services and
             online stores.
           </p>
         </div>
       </div>
-      <div className="right">
-        <div className="bg"></div>
-        <img src={Me} alt="" className="img" />
+      <div className="i-right">
+        <div className="i-bg"></div>
+        <img src={Me} alt="" className="i-img" />
       </div>
     </div>
   );
