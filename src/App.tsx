5cbb2beab1fea34ee73d1594f30d7aad1f154ae9
Mike-Csta <92047998+Mike-Csta@users.noreply.github.com>
2021-10-21 20:42:22 +0200

    loading-page add

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import "./App.css";
 import Intro from "./Intro/Intro";
 import About from "./About/About";
 import LoadingScreen from "./LoadingScreen/LoadingScreen";
-// import Canvass from "./Canvas/Canvas";
+
 function App() {
   return (
     <div className="App">
+      <LoadingScreen />
       <Intro />
       <About />
     </div>
--- src/LoadingScreen/LoadingScreen.tsx
+++ src/LoadingScreen/LoadingScreen.tsx
@@ -1,5 +1,16 @@
+import "./LoadingScreen.scss";
+
 const LoadingScreen = () => {
-  return "dasdas";
+  return (
+    <>
+      <div className="load-container"></div>
+      <div className="load-content">
+        <div className="load-hello">What’s up?</div>
+        <h1 className="load-wait">One Moment</h1>
+      </div>
+      <div className="load-load"></div>
+    </>
+  );
 };
 
 export default LoadingScreen;
