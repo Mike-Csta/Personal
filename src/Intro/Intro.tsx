60151cba0a5b0dfe0d558c9de8bd3c8118a11d21
Mike-Csta <92047998+Mike-Csta@users.noreply.github.com>
2021-10-23 18:27:35 +0200

    Fast edit: delete logo

--- src/Intro/Intro.tsx
+++ src/Intro/Intro.tsx
@@ -48,9 +48,7 @@ const Intro = () => {
 
   return (
     <>
-      <div className="i-logo-absolute">
-        <div className="i-logo">M I K K E</div>
-      </div>
+     
       <div className="i-top">
         <div className="i-container">
           <div className="i-left">
