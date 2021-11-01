4c53b8df1a6eee052f929d75793d7cc3c3398f56
Mike-Csta <92047998+Mike-Csta@users.noreply.github.com>
2021-11-01 20:49:01 +0100

    media bar update

--- src/MediaBar/MbElem.tsx
+++ src/MediaBar/MbElem.tsx
@@ -1,11 +1,13 @@
-const MbElem = (props: { link: string; nick: string }) => {
+const MbElem = (props: { link: string; img: string; nick: string }) => {
   return (
     <div className="mb-elem">
       <div className="mb-left">
-        <img className="mb-photo" src={props.link} />
+        <img className="mb-photo" src={props.img} />
       </div>
       <div className="mb-right">
-        <div className="mb-nick">{props.nick}</div>
+        <a target="_blank" className="mb-link" href={props.link}>
+          <div className="mb-nick">{props.nick}</div>
+        </a>
       </div>
     </div>
   );
--- src/MediaBar/MediaBar.tsx
+++ src/MediaBar/MediaBar.tsx
@@ -1,37 +1,38 @@
 import "./MediaBar.scss";
-import github from "../img/github.png";
 // import MediaElem from "./MediaElem";
 import MbElem from "./MbElem";
 const MediaBar = () => {
   const media = [
     {
-      link: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+      link: "https://github.com/Mike-Csta",
+      img: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
       nick: "mike-csta",
     },
     {
-      link: "https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg",
+      link: "https://www.facebook.com/mikomikomiko203",
+      img: "https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg",
       nick: "mike csta",
     },
     {
-      link: "https://cdn.pixabay.com/photo/2021/06/15/12/17/instagram-6338401__340.png",
+      link: "https://www.instagram.com/mike_csta/?hl=pl",
+      img: "https://cdn.pixabay.com/photo/2021/06/15/12/17/instagram-6338401__340.png",
       nick: "mike_csta",
     },
     {
-      link: "https://cdn.icon-icons.com/icons2/2699/PNG/512/youtube_logo_icon_168737.png",
+      link: "https://www.youtube.com/channel/UCe4pmiysNRQib9Ul2TPUt_w",
+      img: "https://cdn.icon-icons.com/icons2/2699/PNG/512/youtube_logo_icon_168737.png",
       nick: "mike csta",
     },
 
     {
-      link: "https://www.freeiconspng.com/thumbs/logo-twitter-png/logo-twitter-icon-symbol-0.png",
+      link: "https://twitter.com/MIKOAJ28029107",
+      img: "https://www.freeiconspng.com/thumbs/logo-twitter-png/logo-twitter-icon-symbol-0.png",
       nick: "mike_csta",
     },
     {
-      link: "https://cliply.co/wp-content/uploads/2021/02/372102690_TIKTOK_LOGO_1080.png",
+      link: "https://www.tiktok.com/@mikecsta?",
+      img: "https://cliply.co/wp-content/uploads/2021/02/372102690_TIKTOK_LOGO_1080.png",
       nick: "mike csta",
-    },
-    {
-      link: "https://cliply.co/wp-content/uploads/2021/02/372102690_TIKTOK_LOGO_1080.png",
-      nick: "- - -",
     },
   ];
 
@@ -39,12 +40,12 @@ const MediaBar = () => {
     <div className="mb-container">
       <div className="mb-anim1">
         {media.map((e) => (
-          <MbElem link={e.link} nick={e.nick} />
+          <MbElem link={e.link} img={e.img} nick={e.nick} />
         ))}
       </div>
       <div className="mb-anim2">
         {media.map((e) => (
-          <MbElem link={e.link} nick={e.nick} />
+          <MbElem link={e.link} img={e.img} nick={e.nick} />
         ))}
       </div>
     </div>
